7d357b030c15abaa7961e569b259129bad363e27
SumSumCorn <a01035587180@gmail.com>
2022-02-05 21:41:54 +0900

    todoList 추가 기능 직전

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import React, { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '할일1',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '할일2',
+      checked: false,
+    },
+    {
+      id: 3,
+      text: '할일3',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
